999b09e11b1e0457e71bce354e8e4fa44c8789b9
Rahul Ravindran <beingcodr@gmail.com>
2020-06-09 22:16:58 +0530

    Started with User Leveling

---
+++ server/models/Member.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const memberSchema = new Schema({
+    entity: { type: String, required: true },
+    discordId: { type: String, required: true },
+    discriminator: { type: String, required: true },
+    username: { type: String, required: true },
+    nickName: { type: String },
+    avatar: { type: String },
+    server: { type: String, required: true },
+    roles: [{ type: String }],
+    level: { type: Number, required: true },
+    totalPoints: { type: Number, required: true },
+    points: {
+        contribution: { type: Number, required: true },
+        codeError: { type: Number, required: true },
+        verbalDoubt: { type: Number, required: true },
+        codeDoubt: { type: Number, required: true },
+        sharedResource: { type: Number, required: true },
+        slangUsed: { type: Number, required: true },
+    },
+});
+
+module.exports = new mongoose.model('Member', memberSchema);
